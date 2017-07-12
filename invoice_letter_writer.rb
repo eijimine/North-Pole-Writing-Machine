81f0a583941814c3b660272e7d1ba81ea043d9f7
eiji <eijimine@gmail.com>
2017-07-12 16:49:35 -0400

    stuck on ex3

---
+++ invoice_letter_writer.rb
@@ -0,0 +1,63 @@
+require 'erb'
+
+kids_data   = File.read('data/kids-data.txt')
+invoice_letter = File.read('templates/invoice_letter.txt.erb')
+
+kids_data.each_line do |kid|
+
+  kid_data_array = kid.split
+
+  name         = kid_data_array[0]
+  gender       = kid_data_array[1]
+  behavior     = kid_data_array[2]
+  toys         = kid_data_array[3..8]
+  house_number = kid_data_array[9]
+  street_name  = kid_data_array[10]
+  street_type  = kid_data_array[11]
+  postal_code  = kid_data_array[12]
+  house_value  = kid_data_array[13].to_i
+
+toy_hash = {}
+
+  if house_value >= 1000000
+    toy_value = 0
+    sub_total = 0
+    toys.each do |toy|
+      if toy == 'Kaleidoscope'
+        toy_value = 0
+      else
+        toy_value = 1000
+      end
+      sub_total += toy_value
+     toy_hash[toy] = toy_value
+    end
+   elsif house_value < 1000000 && house_value > 200000
+    toy_value = 0
+    sub_total = 0
+    toys.each do |toy|
+      if toy == 'Kaleidoscope'
+        toy_value = 0
+      else
+        toy_value = 100
+      end
+      sub_total += toy_value
+      toy_hash[toy] = toy_value
+    end
+    end
+
+
+
+  # next unless behavior == 'naughty'
+
+
+
+
+  filename    = 'letters/invoices/' + name + '.txt'
+  letter_text = ERB.new(invoice_letter, nil, '-').result(binding)
+
+  puts "Writing #{filename}."
+  File.write(filename, letter_text)
+
+end
+
+puts 'Done!'
--- test.rb
+++ test.rb
@@ -1,18 +1,37 @@
 toys = ['Meccano', 'Gumby', 'Soldier', 'Kaleidoscope', 'Pony', 'Lego' ]
+house_value = 250000
 
-toys.each do |toy|
-  if toy == 'Kaleidoscope'
-    toys.delete(toy)
+toy_hash = {}
+
+if house_value >= 1000000
+  toy_value = 0
+  sub_total = 0
+  toys.each do |toy|
+    if toy == 'Kaleidoscope'
+      toy_value = 0
+    else
+      toy_value = 1000
+    end
+    sub_total += toy_value
+    toy_hash[toy] = toy_value
   end
+elsif house_value < 1000000 && house_value > 200000
+  toy_value = 0
+  sub_total = 0
+  toys.each do |toy|
+    if toy == 'Kaleidoscope'
+      toy_value = 0
+    else
+      toy_value = 100
+    end
+    sub_total += toy_value
+    toy_hash[toy] = toy_value
+  end
+
+
+
 end
 
-puts toys
-toys = toys.shuffle!
-
-random_toy = toys[0]
-
-puts "The random toy is #{random_toy}"
-
-toys.delete(random_toy)
-
-puts "The list of new toys are #{toys}"
+toy_hash.each do |toy, value|
+  puts "#{value} #{toy}"
+end
